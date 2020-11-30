aeb57a744f2fb4451ca2f203a94d7516ef5cf9a1
Suntsov Kirill <Kirill.Suntsov@infotecs.ru>
2020-11-30 13:12:33 +0700

    Починка сжатия неквадратных стикеров

--- Helpers/ImageHelper.cs
+++ Helpers/ImageHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
@@ -20,13 +21,12 @@ namespace HRAshton.CustomStickerExtender.Helpers
 			result.StreamSource = stream;
 			result.EndInit();
 			result.Freeze();
-			
+
 			return result;
 		}
 
 		public static Bitmap ResizeImage(Image image, Size size)
 		{
-			var destRect = new Rectangle(0, 0, size.Width, size.Height);
 			var destImage = new Bitmap(size.Width, size.Height);
 
 			destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
@@ -38,9 +38,20 @@ namespace HRAshton.CustomStickerExtender.Helpers
 			graphics.SmoothingMode = SmoothingMode.HighQuality;
 			graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
+			var imageSize = GetSize(size, image.Size);
+			var destRect = new Rectangle(0, destImage.Height - imageSize.Height, imageSize.Width, imageSize.Height);
+			
 			using var wrapMode = new ImageAttributes();
-			wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-			graphics.DrawImage(image, destRect, 0, 0, image.Width,image.Height, GraphicsUnit.Pixel, wrapMode);
+			wrapMode.SetWrapMode(WrapMode.Tile);
+			graphics.DrawImage(
+				image,
+				destRect,
+				0,
+				0,
+				image.Width,
+				image.Height,
+				GraphicsUnit.Pixel,
+				wrapMode);
 
 			return destImage;
 		}
@@ -49,12 +60,24 @@ namespace HRAshton.CustomStickerExtender.Helpers
 		{
 			var bmp2 = new Bitmap(bmp1.Width, bmp1.Height);
 			var rect = new Rectangle(Point.Empty, bmp1.Size);
-			
+
 			using var g = Graphics.FromImage(bmp2);
 			g.Clear(target);
 			g.DrawImageUnscaledAndClipped(bmp1, rect);
-			
+
 			return bmp2;
+		}
+
+		private static Size GetSize(Size target, Size source)
+		{
+			var wCoeff = (float)target.Width / source.Width;
+			var hCoeff = (float)target.Height / source.Height;
+			var minCoeff = Math.Min(wCoeff, hCoeff);
+			
+			return new Size(
+				width: (int)(source.Width * minCoeff),
+				height: (int)(source.Height * minCoeff)
+			);
 		}
 	}
 }
